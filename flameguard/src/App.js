e406422abb895027688f16e022771292df31b838
buffaloss <beatricia.golban@isa.utm.md>
2024-06-09 18:29:13 +0300

    Add header title for dashboard

--- flameguard/src/App.js
+++ flameguard/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import './index.css'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <Navbar />
+      <Header />
     </div>
   );
 }
