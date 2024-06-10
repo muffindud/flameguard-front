bb7c59c311928d1feeb70da2d5b552ebb85c4be9
buffaloss <beatricia.golban@isa.utm.md>
2024-06-10 13:52:39 +0300

    Add about us page

---
+++ flameguard/src/components/TeamIntro.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../styles/TeamIntro.css";
+
+function TeamIntro() {
+  return (
+    <div className="intro-container">
+      <div className="intro-header">
+        At <span>FlameGuard</span>, we are a team of five passionate students
+      </div>
+      <div className="intro-text">
+        dedicated to learning and striving for excellence with each step of our
+        project journey. With a shared vision for innovation and a commitment to
+        fire safety, we have embarked on a mission to develop a cutting-edge
+        fire detection and monitoring system.
+      </div>
+    </div>
+  );
+}
+
+export default TeamIntro;
